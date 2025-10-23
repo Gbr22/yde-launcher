479e45f3c43343a8148d960b11540bf07db4a532
kess <git@kess.sh>
2025-10-23 20:41:54 +0200

    keep the search input in focus

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,52 @@
-use iced::widget::text_input::Id;
-use iced::widget::{button, text};
 use iced::{Element, Task};
 
 #[derive(Debug, Clone)]
 enum Message {
     Event(iced::Event),
-    Increment,
     SetQuery(String),
-    Focus(Id)
-}
-
-fn exit() {
-    std::process::exit(0);
 }
 
 struct State {
-    counter: u64,
     query: String,
 }
 
 impl Default for State {
     fn default() -> Self {
         Self {
-            counter: 0,
             query: String::new()
         }
     }
 }
 
-pub fn search_id() -> iced::widget::text_input::Id {
-    return iced::widget::text_input::Id::new("search_input");
-}
-
 fn update(state: &mut State, message: Message) -> iced::Task<Message> {
     match message {
-        Message::Increment => state.counter += 1,
         Message::SetQuery(s) => state.query = s,
         Message::Event(iced::Event::Window(iced::window::Event::CloseRequested)) => {
-            exit();
+            return iced::exit();
         },
-        Message::Event(iced::Event::Keyboard(iced::keyboard::Event::KeyPressed { key, .. })) => {
-            if key == iced::keyboard::Key::Named(iced::keyboard::key::Named::Escape) {
-                exit();
-            }
-        },
-        Message::Focus(id) => {
-            return iced::widget::text_input::focus(id);
+        Message::Event(e) => {
+            match e {
+                iced_core::event::Event::Window(iced_core::window::Event::Unfocused) => {
+                    return iced::exit();
+                },
+                iced_core::event::Event::Window(iced_core::window::Event::Focused) => {
+                    return iced::widget::operation::focus("search_input");
+                },
+                iced::Event::InputMethod(iced_core::input_method::Event::Closed) => {
+                    return iced::widget::operation::focus("search_input");
+                },
+                iced::Event::Keyboard(iced::keyboard::Event::KeyPressed { key, .. }) => {
+                    if key == iced::keyboard::Key::Named(iced::keyboard::key::Named::Escape) {
+                        return iced::exit();
+                    }
+                },
+                iced::Event::Keyboard(iced::keyboard::Event::KeyReleased { key, .. }) => {
+                    if key == iced::keyboard::Key::Named(iced::keyboard::key::Named::Escape) {
+                        return iced::exit();
+                    }
+                },
+                _ => {}
+            };
         },
         _ => {}
     };
@@ -55,15 +56,22 @@ fn update(state: &mut State, message: Message) -> iced::Task<Message> {
 
 fn view(state: &State) -> Element<Message> {
     iced::widget::column![
-        button(text(state.counter)).on_press(Message::Increment),
         iced::widget::text_input("Search", &state.query)
             .on_input(|s| Message::SetQuery(s))
-            .id(search_id()),
+            .id("search_input")
+            .style(|theme, status| {
+                let mut s = iced::widget::text_input::default(&theme, status);
+                s.border.color = iced::Color::from_rgba(0.0, 0.0, 0.0, 0.0);
+                s.border.width = 0.0;
+                s
+            })
     ].into()
 }
 
 pub fn main() -> iced::Result {
-    iced::application("yde-launcher", update, view)
+    iced::application(||{
+        State::default()
+    }, update, view)
         .subscription(|_state| iced::event::listen().map(Message::Event))
         .window(iced::window::Settings {
             decorations: false,
