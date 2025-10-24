dcdf1168d5c056b04da0d7bdd1d3da0c7a0632e4
kess <git@kess.sh>
2025-10-25 00:10:07 +0200

    fix nix build

--- flake.nix
+++ flake.nix
@@ -38,6 +38,11 @@
 
           cargoLock = {
             lockFile = ./Cargo.lock;
+            outputHashes = {
+              "cryoglyph-0.1.0" = "1rqk1bz8l1nn3m1qh4ax50ca8lhl07xj2vc0d7xq813r6y3spkr5";
+              "dpi-0.1.1" = "0h4dcyl8s0wiwki7r1l29zkfcy9h6c0q5s7ia4iwj92j46aga2d5";
+              "iced-0.14.0-dev" = "0lp4qqd0zgd0cjy1lmnq8jixha2sb3q3bhw1v29k03k2sfnw4z7b";
+            };
           };
 
           nativeBuildInputs = [ pkgs.pkg-config ];
